9f78f2fada779d975363a86d610c6703def21997
Cory Luitjohan <cluitjohan@gmail.com>
2016-12-15 11:47:53 -0600

    check for url on index page and text

--- music_album/test/acceptance/index_test.js
+++ music_album/test/acceptance/index_test.js
@@ -17,5 +17,12 @@ describe('Express CRUD', () => {
       browser.get('/')
       expect(element(by.tagName('h1')).getText()).toEqual('OMG Albums!')
     })
+    it('Then I see the link text', () => {
+      browser.get('/')
+      expect(element(by.tagName('a')).getText()).toEqual('Let me see the RIGHT NOW')
+      let hrefText = element(by.cssContainingText("a","Let me see the RIGHT NOW")).getAttribute('href')
+      expect(hrefText).toContain('/albums')
+    })
+
   })
 })
